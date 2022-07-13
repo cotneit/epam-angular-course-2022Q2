2c00345b80d321f13a6977e4e617cd758b25a45f
Vladyslav Karasov <vladyslav2k@gmail.com>
2022-07-13 22:17:23 +0300

    feat: add first-component

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+enum Category {
+  Entertainment = 'Entertainment',
+  Education = 'Education',
+  Healthcare = 'Healthcare',
+  WorkFromHome = 'WFH',
+}
+
+enum Genre {
+  Crime = 'Crime',
+  Mystery = 'Mystery',
+  Drama = 'Drama',
+}
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css'],
+})
+export class FirstComponent {
+  constructor() {}
+
+  name = 'A Book';
+  description =
+    'A simple book, that, as any other book, contains pages. The contents of those pages shall remain our secret.';
+  price = 10.99;
+  isAvailable = true;
+  category = Category.Entertainment;
+
+  chapters: string[] = [
+    'Introduction',
+    'The Great Reveal',
+    'So what is this book about?',
+    'What does that even mean?',
+    'To be continued',
+  ];
+
+  genres: Genre[] = [Genre.Crime, Genre.Mystery, Genre.Drama];
+}
