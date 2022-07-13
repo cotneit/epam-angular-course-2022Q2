48831e4f9b9361c7e56e7ab15a135f4af2f01e4b
Vladyslav Karasov <vladyslav2k@gmail.com>
2022-07-13 22:14:46 +0300

    build: add prettier and tailwind

---
+++ .prettierrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  printWidth: 90,
+
+  useTabs: false,
+  tabWidth: 2,
+
+  semi: true,
+
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+
+  trailingComma: 'all',
+
+  bracketSpacing: true,
+  bracketSameLine: false,
+
+  arrowParens: 'always',
+
+  proseWrap: 'preserve',
+
+  // https://prettier.io/docs/en/options.html#html-whitespace-sensitivity
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+
+  endOfLine: 'lf',
+
+  // https://prettier.io/docs/en/options.html#embedded-language-formatting
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: true,
+};
